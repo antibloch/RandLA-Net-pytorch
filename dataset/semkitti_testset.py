1d897277d8277787e49d939445e2fc11934fc532
Junaid Iqbal Khan <dianujkotov15@gmail.com>
2025-04-07 15:54:35 +0500

    Update semkitti_testset.py

--- dataset/semkitti_testset.py
+++ dataset/semkitti_testset.py
@@ -130,7 +130,7 @@ class SemanticKITTI(torch_data.IterableDataset):
         inputs['interp_idx'] = []
         for tmp in flat_inputs[3 * num_layers:4 * num_layers]:
             inputs['interp_idx'].append(torch.from_numpy(tmp).long())
-        inputs['features'] = torch.from_numpy(flat_inputs[4 * num_layers]).transpose(1, 2).float()
+        inputs['features'] = torch.from_numpy(flat_inputs[4 * num_layers]).transpose(1, 2).float().contiguous()
         inputs['labels'] = torch.from_numpy(flat_inputs[4 * num_layers + 1]).long()
         input_inds = flat_inputs[4 * num_layers + 2]
         cloud_inds = flat_inputs[4 * num_layers + 3]
